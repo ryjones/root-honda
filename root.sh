bd9a7714c5163a40131274656698f460a50274d7
Ry Jones <ry@linux.com>
2020-05-14 22:31:31 -0700

    explicit path
    Signed-off-by: Ry Jones <ry@linux.com>

--- root.sh
+++ root.sh
@@ -7,24 +7,24 @@
 ARCH="armeabi-v7a"
 
 echo 'Please connect your computer with the head unit...'
-adb wait-for-device
+~/Library/Android/sdk/platform-tools/adb wait-for-device
 
-adb push libs/$ARCH/dirtycow /data/local/tmp/dcow
-adb shell 'chmod 777 /data/local/tmp/dcow'
+~/Library/Android/sdk/platform-tools/adb push libs/$ARCH/dirtycow /data/local/tmp/dcow
+~/Library/Android/sdk/platform-tools/adb shell 'chmod 777 /data/local/tmp/dcow'
 
-adb push libs/$ARCH/run-as /data/local/tmp/run-as
-adb shell '/data/local/tmp/dcow /data/local/tmp/run-as /system/bin/run-as'
+~/Library/Android/sdk/platform-tools/adb push libs/$ARCH/run-as /data/local/tmp/run-as
+~/Library/Android/sdk/platform-tools/adb shell '/data/local/tmp/dcow /data/local/tmp/run-as /system/bin/run-as'
 
-adb shell 'mkdir /data/local/tmp/rootme/'
-adb push nefarious.sh /data/local/tmp/rootme/
-adb push su /data/local/tmp/rootme/
-adb push SuperSU-v2.78.apk /data/local/tmp/rootme/
+~/Library/Android/sdk/platform-tools/adb shell 'mkdir /data/local/tmp/rootme/'
+~/Library/Android/sdk/platform-tools/adb push nefarious.sh /data/local/tmp/rootme/
+~/Library/Android/sdk/platform-tools/adb push su /data/local/tmp/rootme/
+~/Library/Android/sdk/platform-tools/adb push SuperSU-v2.78.apk /data/local/tmp/rootme/
 
-adb disconnect
+~/Library/Android/sdk/platform-tools/adb disconnect
 echo 'Wait 10 seconds...'
 sleep 10
-adb wait-for-device
+~/Library/Android/sdk/platform-tools/adb wait-for-device
 echo 'OK'
 
-adb shell 'chmod 777 /data/local/tmp/rootme/nefarious.sh'
-adb shell 'echo /data/local/tmp/rootme/nefarious.sh | run-as'
+~/Library/Android/sdk/platform-tools/adb shell 'chmod 777 /data/local/tmp/rootme/nefarious.sh'
+~/Library/Android/sdk/platform-tools/adb shell 'echo /data/local/tmp/rootme/nefarious.sh | run-as'
